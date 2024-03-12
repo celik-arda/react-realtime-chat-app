80874e061b8039e9c76481c5fee5728ec3e86b7c
c-arda <ardacelik.dev@gmail.com>
2024-03-12 22:01:54 +0300

    feat(App.js): add components and socket props
    Signed-off-by: c-arda <ardacelik.dev@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import './App.css';
 import io from 'socket.io-client';
+import LoginScreen from './components/LoginScreen';
+import ChatScreen from './components/ChatScreen';
+
 
 // connect with io to send datas to backend from frontend //
 const socket = io.connect('http://localhost:3001');
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
-  );
+
+    return (
+        <div className="App">
+            
+            <LoginScreen socket={socket}/>
+            
+            <ChatScreen/>
+
+
+        </div>
+    );
 }
 
 export default App;
