5516e950d67a6b98e21bca3fc945d6800ea6efbd
c-arda <ardacelik.dev@gmail.com>
2024-03-16 00:44:40 +0300

    fix(App.js && index.js): solve the contextApi error
    remove ContextProvider from App.js and move to index.js. Wrap the App component with ContextProvider in index.js
    
    Signed-off-by: c-arda <ardacelik.dev@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import io from 'socket.io-client';
 import {useState, useContext} from 'react';
 import {ContextProvider} from './components/contextApi/context';
+import MyContext from './components/contextApi/context';
 
 import LoginScreen from './components/LoginScreen';
 import ChatScreen from './components/ChatScreen';
@@ -12,16 +13,30 @@ const socket = io.connect('http://localhost:3001');
 
 function App() {
 
+    const {
+        username,
+        setUserName,
+        roomId,
+        setRoomId,
+        currentMessage,
+        setCurrentMessage,
+        messageList,
+        setMessageList,
+        loginDisplay,
+        setLoginDisplay
+    } = useContext(MyContext);
 
 
     return (
         <div className="App">
 
-            <ContextProvider>
-                <LoginScreen socket={socket}/>
-                <br/>
-                <ChatScreen socket={socket}/>
-            </ContextProvider>
+            {/* <ContextProvider> */}
+                {loginDisplay ?
+                    (<LoginScreen socket={socket}/>)
+                    :
+                    (<ChatScreen socket={socket}/>)
+                }
+            {/* </ContextProvider> */}
 
         </div>
     );
